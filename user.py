c8c7cac6f278485aee6c3e48afcdc5cf2efe959e
Kennedy-karuri <kennkaruri99@gmail.com>
2020-11-21 00:23:13 +0300

    created class user.py

--- user.py
+++ user.py
@@ -1,14 +1,14 @@
-# class User:
-#     """
-#     Class that generates new instances of users
-#     """
-#     user_list = [] # Empty user list
+class User:
+    """
+    Class that generates new instances of users
+    """
+    user_list = [] # Empty user list
 
-#     def __init__(self,user_name,user_password,):
+    def __init__(self,user_name,user_password,):
 
-#         '''
-#         __init__ method that helps us define properties for our objects.
-#         '''
+        '''
+        __init__ method that helps us define properties for our objects.
+        '''
 
-#         self.user_name = user_name
-#         self.user_password = user_password
+        self.user_name = user_name
+        self.user_password = user_password
