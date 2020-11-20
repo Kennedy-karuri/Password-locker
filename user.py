be5ed487764869b5991657ed792ded7c11a61045
Kennedy-karuri <kennkaruri99@gmail.com>
2020-11-21 00:22:04 +0300

    initial commit

---
+++ user.py
@@ -0,0 +1,14 @@
+# class User:
+#     """
+#     Class that generates new instances of users
+#     """
+#     user_list = [] # Empty user list
+
+#     def __init__(self,user_name,user_password,):
+
+#         '''
+#         __init__ method that helps us define properties for our objects.
+#         '''
+
+#         self.user_name = user_name
+#         self.user_password = user_password
