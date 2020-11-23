b928f2a34f06c4b64f88e44516f9287b884447a1
Kennedy-karuri <kennkaruri99@gmail.com>
2020-11-23 08:52:47 +0300

    modified user .py file

--- user.py
+++ user.py
@@ -43,71 +43,39 @@ class Credentials:
     """
     credentials_list = [] # Empty credentials list
 
-    def __init__(self,c_name,c_password,):
+    def __init__(self,account,user_name,user_password):
 
         '''
         __init__ method that helps us define properties for our objects.
         '''
+        self.account = account
+        self.user_name = user_name
+        self.user_password = user_password
 
-        self.c_name = c_name
-        self.c_password = c_password
-
-    @classmethod
-    def verify_user(cls,user_name,password):
-        entered_default = ""
-        for user in User.user_list:
-            if(user.user_name == user_name and user.password == password):
-                entered_default == user.user_name
-        return entered_default
-
-    def save_user_credentials(self):
-        """
-        save_user_credentials method saves a new user object to credentials list
-
-        """
-
-        Credentials.credentials_list.append(self)     
+    def save_credentials(self):
+        Credentials.credentials_list.append(self)
 
     def delete_credentials(self):
-            """
-            delete_credentials method deletes a saved credential from the credentials_list
-
-            """   
-
-            Credentials.credentials_list.remove(self)
+        Credentials.credentials_list.append(self)
 
     @classmethod
-    def find_by_number(cls,user_name): 
-        """
-        Method that takes in a password and returns a password that matches the password.
-
-        """  
-        for credentials in cls.credentials_list:
-            if (credentials.c_username == user_name):
-                return credentials
-    
-
-    @classmethod
-    def credentials_exist(cls,user_name):
-        """
-        Method that checks if a credential exists from the user details list.
-        Args:
-        number: Phone number to search if the user details exists
-        Returns :
-        Boolean : True or false depending on the user details existance
-
-        """
-        for credentials in cls.credentials_list:
-            if credentials.c_username == user_name:
+    def credential_user_exist(cls,account):
+        for credential in cls.credentials_list:
+            if credential.account == account:
                 return True
-                  
         return False
 
-    def generate_password(self):
-        """
-        generate a random password consisting of letters
+    @classmethod
+    def find_account(cls,account):
+        for credential in cls.credentials_list:
+            if credential.account == account:
+                return credential
+    
+    @classmethod
+    def show_credentials(cls):
+        return cls.credentials_list
 
-        """
+    def generate_password (string_len = 5):
+                        password = string.ascii_uppercase + string.ascii_lowercase + string.digits + "(|/~!.@,)#{?&[%]^}&*"
+                        return ''.join(random.choice(password) for i in range(string_len))
 
-        password = string.ascii_lowercase + string.ascii_uppercase + "havertz"
-        return''.join(random.choice(password)for i in range(1,9))
